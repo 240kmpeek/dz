1dff31898705e867b4b8925375918d8b84bf2906
240kmpeek <mmmmmmmmo9859@gmail.com>
2023-10-07 14:31:21 +0300

    dz 1

--- dz/dz1.py
+++ dz/dz1.py
@@ -1,11 +1,13 @@
 import colorama
 
-
+# Список стилей колорама
 available_styles = colorama.Style.__dict__.keys()
 print("Available styles:", available_styles)
 
+# Список цветов текста
 available_text_colors = colorama.Fore.__dict__.keys()
 print("Available text colors:", available_text_colors)
 
+# Список задних фонов
 available_background_colors = colorama.Back.__dict__.keys()
 print("Available background colors:", available_background_colors)
